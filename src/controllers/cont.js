f147e72cd218fb9060b05360835ed928c297afc6
lemachegabriel <lemache35@gmail.com>
2022-06-17 21:09:23 -0300

    taeta

--- src/controllers/cont.js
+++ src/controllers/cont.js
@@ -4,9 +4,19 @@ module.exports = {
         res.sendStatus(200);
     }, 
     async getTest(req, res) {
-        const data = req.query
-        const challenge = data['hub.challenge']
-        console.log(data)
-        res.status(200).send(challenge);
+        const verify_token = process.env.VERIFY_TOKEN;
+
+        let mode = req.query["hub.mode"];
+        let token = req.query["hub.verify_token"];
+        let challenge = req.query["hub.challenge"];
+
+        if (mode && token) {
+            if (mode === "subscribe" && token === verify_token) {
+                console.log("WEBHOOK_VERIFIED");
+                res.status(200).send(challenge);
+            } else {
+                res.sendStatus(403);
+            }
+        }
     }
 }
